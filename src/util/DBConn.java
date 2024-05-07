9d24a88e67d0d0f0f01806bcbe10ce8fed8c3b26
soyoungxxx <100336643+soyoungxxx@users.noreply.github.com>
2024-05-07 09:23:24 +0900

    Update DBConn.java

--- src/util/DBConn.java
+++ src/util/DBConn.java
@@ -10,9 +10,9 @@ public class DBConn {
     public static Connection getConnection() {
         try {
             if (dbConn == null) {
-                String url = "jdbc:oracle:thin:@hb-database.cl6k8mkm2m29.us-east-1.rds.amazonaws.com:1521:ORCL";
-                String user = "admin";
-                String pwd = "admin1234";
+                String url = "";
+                String user = "";
+                String pwd = "";
 
                 Class.forName("oracle.jdbc.driver.OracleDriver");
                 dbConn = DriverManager.getConnection(url, user, pwd);
